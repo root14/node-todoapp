9056f79335f5bbc50456978c68027d1d6efed93b
Basri İlkay GAVAZ <ilkaygavaz2@gmail.com>
2024-07-29 12:46:04 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+//npm run dev
+import express from "express"
+
+const app = express()
+const router = express.Router()
+
+
+app.get("/", (req, res) => {
+    res.json({
+        "hey": "douglas!"
+    })
+})
+
+
+app.listen(3000, () => console.log("server running"))
