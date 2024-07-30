80f59ebe22e3f8eccedcc227ec49fcf0a2e671cf
Basri İlkay GAVAZ <ilkaygavaz2@gmail.com>
2024-07-31 00:41:18 +0300

    add jwt

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import { prisma } from '../app';
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const register = async (req: Request, res: Response) => {
     try {
@@ -46,9 +50,23 @@ const login = async (req: Request, res: Response) => {
             bcrypt.compare(plainPassword, storedUser.hashedPassword, (err, isMatch) => {
                 if (err) { res.status(403).json({ error: err.message }) }
                 if (isMatch) {
-                    res.status(200).json({
-                        succes: true,
+                    const token = jwt.sign({
+                        email: storedUser.email,
                         userName: storedUser.userName
+                    }, process.env.JWT_SECRET_KEY as string, { expiresIn: 60 * 60 })
+                    prisma.user.update({
+                        where: {
+                            email: email,
+                        },
+                        data: {
+                            token: token
+                        },
+                    }).then((updatedUser) => {
+                        res.status(200).json({
+                            succes: true,
+                            userName: storedUser.userName,
+                            jwt: token
+                        })
                     })
                 } else {
                     res.status(200).json({
